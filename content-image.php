0ab38b549068936d720cfe9899c0bcc4f7d7f522
Kimberley <kimberley@techarchx.com>
2013-03-30 03:56:11 -0300

    Create content-image.php

---
+++ content-image.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying posts in the Image post format.
+ *
+ * @package WordPress
+ * @subpackage Blank_Cartridge
+ * @since Blank Cartridge 1.0
+ */
+?>
+
+<?php the_ID(); ?>" <?php post_class(); ?>
+
+<?php the_image( 'blankcartridge-image-post' ); ?>
+
+  
+<?php if ( is_single() ) : ?>
+<?php the_title(); ?>
+<?php else : ?>
+
+<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'blankcartridge' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?>
+	
+<?php endif; // is_single() ?>
+	
+	
+<?php the_remaining_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'blankcartridge' ) ); ?>
+<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'blankcartridge' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+	
+
+<?php blankcartridge_entry_meta(); ?>
+
+<?php if ( comments_open() && ! is_single() ) : ?>
+		
+<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'blankcartridge' ) . '</span>', __( 'One comment so far', 'blankcartridge' ), __( 'View all % comments', 'blankcartridge' ) ); ?>
+
+<?php endif; // comments_open() ?>
+<?php edit_post_link( __( 'Edit', 'blankcartridge' ), '<span class="edit-link">', '</span>' ); ?>
+
+<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
+<?php get_template_part( 'author-bio' ); ?>
+<?php endif; ?>
